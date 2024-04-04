0082ca8cd597df291e273198af7ce5fbb14bf009
nihad-abbasov <nihadhadabbasov2000@gmail.com>
2024-04-04 20:53:15 +0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-test2.netlify.app/'
-})
+  site: 'https://astro-test2.netlify.app',
+  integrations: [preact()]
+});
